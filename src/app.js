5ef595ca69c859aa882928767de700cd1308a9a8
LuiisLiins <143644981+LuiisLiins@users.noreply.github.com>
2023-09-28 11:03:43 -0300

    Add files via upload
    Alteramos o App.js colocando um expressão passando o name

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
+import React from 'react';
 
 function App() {
+
+    const [name, setName] = React.useState('Luis Lins');
 
     return (
         <div className='container'>
             <div className='container-login'>
                 <div className='wrap-login'>
-                    
+                    <p>Meu nome é {name}</p>
                 </div>
             </div>
         </div>    
